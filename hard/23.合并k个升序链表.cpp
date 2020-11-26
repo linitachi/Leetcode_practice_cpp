970ffb5de961f9de5c439effb6356f4a59f4de66
linitachi <linitachi@gmail.com>
2020-11-26 21:56:07 +0800

    upload hard 23

---
+++ hard/23.合并k个升序链表.cpp
@@ -0,0 +1,106 @@
+/*
+ * @lc app=leetcode.cn id=23 lang=cpp
+ *
+ * [23] 合并K个升序链表
+ *
+ * https://leetcode-cn.com/problems/merge-k-sorted-lists/description/
+ *
+ * algorithms
+ * Hard (53.45%)
+ * Likes:    1009
+ * Dislikes: 0
+ * Total Accepted:    188.3K
+ * Total Submissions: 351.3K
+ * Testcase Example:  '[[1,4,5],[1,3,4],[2,6]]'
+ *
+ * 给你一个链表数组，每个链表都已经按升序排列。
+ *
+ * 请你将所有链表合并到一个升序链表中，返回合并后的链表。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：lists = [[1,4,5],[1,3,4],[2,6]]
+ * 输出：[1,1,2,3,4,4,5,6]
+ * 解释：链表数组如下：
+ * [
+ * ⁠ 1->4->5,
+ * ⁠ 1->3->4,
+ * ⁠ 2->6
+ * ]
+ * 将它们合并到一个有序链表中得到。
+ * 1->1->2->3->4->4->5->6
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：lists = []
+ * 输出：[]
+ *
+ *
+ * 示例 3：
+ *
+ * 输入：lists = [[]]
+ * 输出：[]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * k == lists.length
+ * 0 <= k <= 10^4
+ * 0 <= lists[i].length <= 500
+ * -10^4 <= lists[i][j] <= 10^4
+ * lists[i] 按 升序 排列
+ * lists[i].length 的总和不超过 10^4
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+ public:
+  ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    if (l1 == NULL) {
+      return l2;
+    }
+    if (l2 == NULL) {
+      return l1;
+    }
+    if (l1->val < l2->val) {
+      l1->next = mergeTwoLists(l1->next, l2);
+      return l1;
+    } else {
+      l2->next = mergeTwoLists(l1, l2->next);
+      return l2;
+    }
+  }
+  ListNode* mergeKLists(vector<ListNode*>& lists) {
+    if (lists.empty()) {
+      return NULL;
+    }
+    int list_length = lists.size();
+    int interval = 1;
+    while (interval < list_length) {
+      for (int i = 0; i < list_length - interval; i += interval * 2) {
+        lists[i] = mergeTwoLists(lists[i], lists[i + interval]);
+      };
+      interval *= 2;
+    }
+    return *lists.begin();
+  }
+};
+// @lc code=end
